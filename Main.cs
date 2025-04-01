1ba11824f0a3e6474a5b8664116807cb812edbf2
SUKRITH .K <ksukrith50@gmaIL.COM>
2025-04-01 21:29:17 +0530

    Create Main.cs

---
+++ Main.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondLargestnum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welxome");
+            Console.WriteLine(" " + args[0]);
+            Console.WriteLine(" " + args[1]);
+            Console.ReadLine();
+        }
+    }
+
+}
