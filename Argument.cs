0f4a7d5d2a48ebd6b2b2d2c03fb4a4882feb1b04
SUKRITH .K <ksukrith50@gmaIL.COM>
2025-04-01 21:56:05 +0530

    Create Argument.cs

---
+++ Argument.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Second
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double a, b;
+            if (args.Length == 0)
+            {
+                Console.WriteLine("no argument");
+            }
+            else
+            {
+                a = double.Parse(args[0]);
+                b = double.Parse(args[1]);
+                double sum = 0, sub = 0, mul = 0, div = 0;
+                sum = a + b;
+                Console.WriteLine("the sum of{0}and{1}is:{2}", a, b, sum);
+                sub = a - b;
+                Console.WriteLine("the sum of{0}and{1}is:{2}", a, b, sub);
+                mul = a * b;
+                Console.WriteLine("the sum of{0}and{1}is:{2}", a, b, mul);
+                div = a / b;
+                Console.WriteLine("the sum of{0}and{1}is:{2}", a, b, div);
+                Console.ReadLine();
+            }
+        }
+    }
+
+}
